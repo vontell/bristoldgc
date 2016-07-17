5825f5dd23bd585bfc48f0bdf5d21cee00351473
Aaron Vontell <vontell@mit.edu>
2016-07-17 16:34:59 -0400

    Initial UI commit

---
+++ src/main/java/com/avontell/BristoldgcApplication.java
@@ -0,0 +1,12 @@
+package com.avontell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BristoldgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BristoldgcApplication.class, args);
+	}
+}
